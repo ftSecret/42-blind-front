7242bd1ed1b2f0faef77d11b041a64e88a674a18
idjaeha <jaehanim1@gmail.com>
2021-12-31 02:23:37 +0900

    BLND-16 데이터를 불러오는 코드 예시 주석 처리

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,23 +1,22 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Cards from 'components/Cards/Cards';
 import Button from 'components/Button/Button';
 import MainHeader from 'components/MainHeader/MainHeader';
 import classes from './MainPage.module.css';
-import { usePostBlindBoardQuery } from 'api/blindBoard';
 
 const MainPage = () => {
   const onClick = () => {
     console.log('clicked');
   };
 
-  // TODO: 해당 코드는 추후에 삭제될 예정
-  const { data, error, isLoading } = usePostBlindBoardQuery('blindBoardAPI');
-  useEffect(() => {
-    if (isLoading === true) console.log('로딩 중...');
-    if (error) console.log(error);
-    if (data) console.log(data);
-  }, [data, error, isLoading]);
+  // TODO: 해당 주석은 추후에 삭제될 예정
+  // const { data, error, isLoading } = usePostBlindBoardQuery('blindBoardAPI');
+  // useEffect(() => {
+  //   if (isLoading === true) console.log('로딩 중...');
+  //   if (error) console.log(error);
+  //   if (data) console.log(data);
+  // }, [data, error, isLoading]);
 
   return (
     <section>
