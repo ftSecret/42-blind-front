637ae6f78cae1d2befe716d3f3a612843666fb06
idjaeha <jaehanim1@gmail.com>
2022-01-02 02:13:33 +0900

    ADMN-14 MyPage를 styled-components로 이전

--- src/components/MyHeader/MyHeader.tsx
+++ src/components/MyHeader/MyHeader.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import classes from 'components/MyHeader/MyHeader.module.css';
+import styled from 'styled-components';
 
 const MyHeader = () => {
   return (
-    <div className={classes.container}>
-      <h1 className={classes.title}>
+    <StyledContainer>
+      <StyledTitle>
         <Link to="/">42 BLIND</Link>
-      </h1>
-    </div>
+      </StyledTitle>
+    </StyledContainer>
   );
 };
 
 export default MyHeader;
+
+const StyledContainer = styled.div`
+  background-color: ${({ theme }) => theme.colors.primary};
+  display: flex;
+  align-items: center;
+  padding: 0 1em;
+`;
+
+const StyledTitle = styled.h1`
+  box-sizing: border-box;
+  width: 100%;
+  font-size: 2.5rem;
+  font-weight: bold;
+  line-height: 1.2;
+  text-align: center;
+`;
