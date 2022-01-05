c305fb020dbcb95007b7f471d7271bf7b887c94d
idjaeha <jaehanim1@gmail.com>
2022-01-05 22:15:57 +0900

    BLND-32 load가 여러번 호출되는 현상 제거

--- src/components/organisms/MainPage/MainBoard/MainBoard.tsx
+++ src/components/organisms/MainPage/MainBoard/MainBoard.tsx
@@ -1,14 +1,14 @@
 import Board from 'components/molecules/Board/Board';
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { getDummies, PostCardType } from 'utils/getDummies';
 
 const MainBoard = () => {
   const [items, setItems] = useState<PostCardType[]>([]);
 
-  const load = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+  const load = useCallback(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 300));
     setItems((items) => [...items, ...getDummies()]);
-  };
+  }, []);
 
   return <Board items={items} load={load} />;
 };
