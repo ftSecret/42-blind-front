e456cf95c70d9d5d32bef50b8fa4f0a7eb175309
idjaeha <jaehanim1@gmail.com>
2022-01-14 00:10:52 +0900

    BLND-63 UTC가 아니면 9시간을 추가하는 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import AppRouter from 'components/utils/AppRouter';
+import { formatDate } from 'utils/formatDate';
+
+console.log('result', formatDate(new Date().toISOString()));
 
 function App() {
   return <AppRouter />;
--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,24 +1,26 @@
 import dayjs from 'dayjs';
-import 'dayjs/locale/ko';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import utc from 'dayjs/plugin/utc';
 
-dayjs.locale('ko');
 dayjs.extend(relativeTime);
+dayjs.extend(utc);
 
-export const formatDate = (stringDate: string) => {
+export const formatDate = (maybeUTCDate: string) => {
   const now = dayjs();
-  const target = dayjs(stringDate);
+  const target = dayjs(maybeUTCDate);
   const hoursAgo24 = now.subtract(1, 'day');
-  const timeDiff = Math.floor(now.diff(target) / 60000); //분
+  const minuteDiff = Math.floor(now.diff(target) / 60000); //분
 
   if (target.isAfter(hoursAgo24)) {
-    if (1 < timeDiff) {
-      //1분 이후부터 -> (1시간 전 ~ 23시간 전, 59분 전 ~ 2분 전)
+    if (minuteDiff >= 1) {
+      //1분 이후부터 -> (1시간 전 ~ 23시간 전, 59분 전 ~ 1분 전)
       return `${target.from(now)}`;
     } else {
       //1분 이내의 시간은 방금 전으로 표시
       return '방금 전';
     }
   }
+
+  if (target.isUTC() === false) target.add(9, 'hour');
   return target.format('MM/DD HH:MM');
 };
