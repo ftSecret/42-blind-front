7351d323d6da32f5ec9d1d03718d44662db0bd99
6mn12j <dbwk0404@hanmail.net>
2022-01-13 16:08:38 +0900

    BLND-63 시간 포맷팅 변경
    24시간 이전에는 기존에 정해진 약속에 맞춰 표기하고
    24시간 이후에는 해당 날짜와 시간을 보여줍니다. 'MM/DD HH:MM'

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,24 @@
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
-dayjs.locale('ko');
+import relativeTime from 'dayjs/plugin/relativeTime';
 
-export const formatDate = (date: string) => {
-  return dayjs(date).format('MM/DD HH:MM');
+dayjs.locale('ko');
+dayjs.extend(relativeTime);
+
+export const formatDate = (stringDate: string) => {
+  const now = dayjs();
+  const target = dayjs(stringDate);
+  const hoursAgo24 = now.subtract(1, 'day');
+  const timeDiff = Math.floor(now.diff(target) / 60000); //분
+
+  if (target.isAfter(hoursAgo24)) {
+    if (1 < timeDiff) {
+      //1분 이후부터 -> (1시간 전 ~ 23시간 전, 59분 전 ~ 2분 전)
+      return `${target.from(now)}`;
+    } else {
+      //1분 이내의 시간은 방금 전으로 표시
+      return '방금 전';
+    }
+  }
+  return target.format('MM/DD HH:MM');
 };
