825b8fc55fd4cb326b17edd60dbefa80a93a885f
idjaeha <jaehanim1@gmail.com>
2022-01-13 16:58:36 +0900

    BLND-60 토큰 발급 로직 추가

--- src/api/blindPost.ts
+++ src/api/blindPost.ts
@@ -15,10 +15,42 @@ import {
   GetPostDetailRequestType,
   GoodPostRequestType,
 } from './type';
+import { Cookies } from 'react-cookie';
+import axios from 'axios';
+import { PATH_LOGIN } from 'components/utils/AppRouter';
+
+const prepareToken = async (headers: Headers) => {
+  const cookies = new Cookies();
+  console.log('[before]', cookies);
+
+  if (cookies.get('refresh') === undefined) {
+    // 로그인 화면으로 이동
+    window.location.href = `https://42blind.com${PATH_LOGIN}`;
+  } else if (cookies.get('jwt') === undefined) {
+    // 헬스체크 api 호출
+    if (env.url.blindAPI) {
+      try {
+        const test = await axios.get(env.url.blindAPI);
+        console.log(test);
+      } catch {
+        // throw new Error('jwt를 가져오는데 실패했습니다.');
+      }
+    }
+  }
+  headers.set('cookies', cookies.get('jwt'));
+  console.log('[document.cookie]', document.cookie);
+  console.log('[jwt]', cookies.get('jwt'));
+  console.log('[after]', new Cookies());
+  return headers;
+};
 
 export const blindPostAPI = createApi({
   reducerPath: 'blindPostAPI',
-  baseQuery: fetchBaseQuery({ baseUrl: env.url.blindAPI, credentials: 'include' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: env.url.blindAPI,
+    credentials: 'include',
+    prepareHeaders: prepareToken,
+  }),
   endpoints: (builder) => ({
     getBlindPost: builder.query<ResponseType<APIPostType[]>, GetPostRequestType>({
       query: ({ size, page }) => `post?size=${size}&page=${page}`,
