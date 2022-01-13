6148db62a98bb8daddd7cd333ee007bd7760193c
idjaeha <jaehanim1@gmail.com>
2022-01-13 15:29:42 +0900

    BLND-54 최상위 댓글을 찾는 로직 추가

--- src/components/molecules/Comments/utils.ts
+++ src/components/molecules/Comments/utils.ts
@@ -34,20 +34,24 @@ export const insertNickname = (rawComments: CommentType[]) => {
   2. 댓글, 답글은 생성 순서대로 정렬합니다.
   */
 export type TempCommentTypes = { reply: CommentType[] } & CommentType;
+const getRootParentId = (comments: CommentType[], comment_id: number): number => {
+  const target = comments.find((c) => c.comment_id === comment_id);
+  if (target === undefined) throw new Error('부모 댓글을 찾을 수 없습니다.');
+  if (target.parent_id === -1) return target.comment_id;
+  return getRootParentId(comments, target.parent_id);
+};
 export const sortComments = (comments: CommentType[]) => {
   const tempComments: TempCommentTypes[] = [];
   // 시간 순서대로 정렬
   comments.sort((a, b) => (dayjs(a.created_at).isAfter(dayjs(b.created_at)) ? 1 : -1));
 
-  // reply 추가
-  comments.forEach((comment) => tempComments.push({ ...comment, reply: [] }));
-
   // 답글은 댓글의 자식으로 이동
-  tempComments.forEach((comment) => {
+  comments.forEach((comment) => {
     if (comment.parent_id !== -1) {
-      const idx = tempComments.findIndex((elem) => elem.comment_id === comment.parent_id);
-      tempComments[idx].reply.push({ ...comment });
-      tempComments.splice(idx, 1);
+      const rootParentId = getRootParentId(comments, comment.comment_id);
+      tempComments.find((comment) => comment.comment_id === rootParentId)?.reply.push(comment);
+    } else {
+      tempComments.push({ ...comment, reply: [] });
     }
   });
 
