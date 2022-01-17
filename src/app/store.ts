625cca8ad745b8752c9cef193bd8e34e74998d91
idjaeha <jaehanim1@gmail.com>
2022-01-17 19:11:02 +0900

    redux devTool이 local에서만 동작하게 변경

--- src/app/store.ts
+++ src/app/store.ts
@@ -5,6 +5,7 @@ import themeReducer, { themeMiddleware } from 'features/theme/themeSlice';
 import { blindPostAPI } from 'api/blindPost';
 import { blindCommentAPI } from 'api/blindComment';
 export const store = configureStore({
+  devTools: process.env.REACT_APP_ENV === 'local' ? true : false,
   reducer: {
     theme: themeReducer,
     user: userReducer,
