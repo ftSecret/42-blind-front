abe1c96f7627e748f3deedae85cbd1bbd4d68656
idjaeha <jaehanim1@gmail.com>
2022-01-19 16:39:40 +0900

    BLND-84 수정 시, 알림 창 삭제
    Co-authored-by: 6mn12j <dbwk0404@hanmail.net>

--- src/components/atoms/icons/AddIcon.tsx
+++ src/components/atoms/icons/AddIcon.tsx
@@ -3,7 +3,6 @@ import CustomIcon from 'components/atoms/icons/CustomIcon';
 import { CustomIconPropTypes } from 'components/atoms/icons/CustomIcon';
 
 const AddIcon = (props: Omit<CustomIconPropTypes, 'iconName'>) => {
-  console.log('icon');
   return <CustomIcon iconName="add" {...props} />;
 };
 
