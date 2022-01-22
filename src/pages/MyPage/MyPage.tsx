3f19379946fe33f3aaf2af74db3a579d531f4628
idjaeha <jaehanim1@gmail.com>
2022-01-22 17:15:06 +0900

    BLND-122 스와이프를 통한 화면 전환

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,30 +1,49 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import MyHeader from 'components/organisms/MyPage/MyHeader';
 import MyTabBar from 'components/organisms/MyPage/MyTabBar';
-import { containerStyle } from 'styles/mixin';
+import { containerStyle, flexColumn } from 'styles/mixin';
 import styled from 'styled-components';
 import MyCommentBoard from 'components/organisms/MyPage/MyCommentBoard';
 import MyPostBoard from 'components/organisms/MyPage/MyPostBoard';
+import { useSwipeable } from 'react-swipeable';
 
 const MyPage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const swipeHandler = useSwipeable({
+    onSwipedRight: () => {
+      if (location.pathname.includes('/comment')) navigate('/my/post', { replace: true });
+      else if (location.pathname.includes('/post')) navigate(-1);
+    },
+    onSwipedLeft: () => {
+      if (location.pathname.includes('/post')) navigate('/my/comment', { replace: true });
+    },
+  });
+
   return (
-    <div>
+    <StyledMyPageContainer>
       <MyHeader />
       <MyTabBar />
-      <StyledContainer>
+      <StyledContainer {...swipeHandler}>
         <Routes>
           <Route path="/" element={<Navigate to="/my/post" replace />} />
           <Route path="/post" element={<MyPostBoard />} />
           <Route path="/comment" element={<MyCommentBoard />} />
         </Routes>
       </StyledContainer>
-    </div>
+    </StyledMyPageContainer>
   );
 };
 
+const StyledMyPageContainer = styled.div`
+  ${flexColumn}
+  height: 100vh;
+`;
+
 const StyledContainer = styled.div`
   ${containerStyle}
+  flex:auto;
 `;
 
 export default MyPage;
